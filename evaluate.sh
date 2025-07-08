e0496cb31c56bb6c0aeacf9e15e6de468b452ab6
Fan <daniel41810@gmail.com>
2025-07-08 15:46:34 +0800

    update evaluate and train.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,12 +1,36 @@
+
+echo "Evaluating AI vs. Human..."
 python -m main --dataset avh
+
+echo "Evaluating Forest Cover Type..."
 python -m main --dataset ct
+
+echo "Evaluating Credit Score..."
 python -m main --dataset cs
+
+echo "Evaluating Fashion MNIST..."
 python -m main --dataset fm
+
+echo "Evaluating Fraud Detection..."
 python -m main --dataset fd
+
+echo "Evaluating MAGIC Gamma Telescope..."
 python -m main --dataset gamma
+
+echo "Evaluating Letter Recognition..."
 python -m main --dataset lr
+
+echo "Evaluating MNIST..."
 python -m main --dataset mnist
+
+echo "Evaluating NEO..."
 python -m main --dataset neo
+
+echo "Evaluating Rice Type..."
 python -m main --dataset rt
+
+echo "Evaluating Smoke Detection..."
 python -m main --dataset sd
+
+echo "Evaluating Tweets..."
 python -m main --dataset tweets
---
+++ train.sh
@@ -0,0 +1,37 @@
+
+
+echo "Training AI vs. Human..."
+python datasets/ai_vs_human/train.py
+
+echo "Training Cover Type..."
+python datasets/cover_type/train.py
+
+echo "Training Credit Score..."
+python datasets/credit_score/train.py
+
+echo "Training Fashion MNIST..."
+python datasets/fashion_mnist/train.py
+
+echo "Training Fraud Detection..."
+python datasets/fraud_detection/train.py
+
+echo "Training Gamma..."
+python datasets/gamma/train.py
+
+echo "Training Letter Recognition..."
+python datasets/letter_recognition/train.py
+
+echo "Training MNIST..."
+python datasets/mnist/train.py
+
+echo "Training NEO..."
+python datasets/neo/train.py
+
+echo "Training Rice Type..."
+python datasets/rice_type/train.py
+
+echo "Training Smoke Detection..."
+python datasets/smoke_detection/train.py
+
+echo "Training Tweets..."
+python datasets/tweets/train.py
