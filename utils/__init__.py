0558f90baec0be47e893bd2f76a21c98af27f8de
Fan Wang <daniel41810@gmail.com>
2026-07-22 08:17:31 +0800

    feat: export EPRDS uncertainty methods

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,25 @@
 from .arguments import parse_args
 from .build_model import load_data
 from .inference import run_mc_dropout, compute_truth_flags
-from .uncertainty_quantification import misclassification_probability, entropy, entropy_mean, mean_entropy, max_entropy, std_predicted_prob
+from .uncertainty_quantification import (
+    misclassification_probability,
+    entropy,
+    entropy_mean,
+    mean_entropy,
+    max_entropy,
+    std_predicted_prob,
+    predicted_vs_rest_entropy,
+    predicted_vs_rest_entropy_deviation,
+    eprds,
+    M_E,
+    E_M,
+    Max_E,
+    DPkP,
+    DPP,
+    EPR,
+    EPRD,
+    EPRDS,
+)
 from .metrics import (
     build_bins_from_arrays,
     summarise_metric_diffs,
